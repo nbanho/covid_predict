7eaafeec2df551baff4859f3acb83f10b600bf87
nbanho <nbanho@ethz.ch>
2022-04-20 11:20:12 +0200

    testing experiments

--- helper/prep.r
+++ helper/prep.r
@@ -5,3 +5,14 @@ cbind.fill <- function(...){
   do.call(cbind, lapply(nm, function (x) 
     rbind(x, matrix(, n-nrow(x), ncol(x))))) 
 }
+
+# create long data frame
+mat_to_long <- function(M, name) {
+  TM <- t(M)
+  colnames(TM) <- seq(1:length(colnames(TM)))
+  D <- data.frame(TM) %>%
+    gather() %>%
+    mutate(key = gsub("X", "", key) %>% as.numeric) %>%
+    mutate(var = name) %>%
+    dplyr::select(var, key, value)
+}
