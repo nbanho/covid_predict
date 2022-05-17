a9b976282696a1a75e2a3fbb9bb18dd42aa75486
nbanho <nbanho@ethz.ch>
2022-05-17 12:19:16 +0200

    update forecast plots to new prediction files

--- utils/eval.r
+++ utils/eval.r
@@ -3,46 +3,48 @@ library(pROC)
 library(tidybayes)
 library(covidcast)
 
+# read state id from file name
+read_state_id <- function(f) { gsub("(_.*)", "", basename(f)) }
 
-# read files
-read_files <- function(f) { do.call(rbind, map(f, readRDS)) }
+# read model from file name
+read_model <- function(f) { gsub(".rds", "", gsub("(.*_)", "", basename(f))) }
 
-# determine maximum days ahead forecast
-# get_max_N <- function(D, model = "arima") { D %>% rowwise() %>% mutate(max_N = ifelse(is.null(dim(!! sym(model))), 1, nrow(!! sym(model)))) }
-
-# get n_ahead forecast draws by model
-get_n_ahead <- function(D, n = 10) {
-  D$data <- map2(D$data, D$forecast_date, function(d,f) dplyr::filter(d, date == f %m+% days(n-1)))
-  D %>% dplyr::filter(sapply(D$data, nrow) > 0)
-}
-
-# convert matrix of draws to list
-mat_to_list <- function(D, models = c("arima", "cori", "prophet", "gp")) {
-  lapply(D$data, function(d) mutate_at(d, vars(models), ~ list(c(.)))) %>%
-    do.call(rbind, .)
-}
-
-mat_to_list_rowwise <- function(D, models = c("arima", "cori", "prophet", "gp")) {
-  # matrix rowwise to list
-  A = lapply(D$data, function(d) lapply(seq_len(nrow(d)), function(i) d[i,]))
-  # flatten list
-  A = A[[1]]
-  # matrix of draws to list
-  B = lapply(A, function(d) mutate_at(d, vars(models), ~ list(c(.)))) 
-  # cbind tibbles
-  C = do.call(rbind, B)
-  # return df
-  return(C)
-}
-
-# get mean forecast
-get_mean_forecast <- function(D, models = c("arima", "cori", "prophet", "gp")) {
-  map2(D$data, D$forecast_date, function(d,f) {
-    d %>%
-      mutate_at(vars(models), ~ apply(., 1, mean)) %>%
-      mutate(n = as.numeric(date - f + 1))
-    }) %>%
-    do.call(rbind, .)
+# read forecasts
+read_forecasts <- function(files) {
+  df_F <- do.call(rbind, lapply(files, function(f) {
+    # read model name
+    m <- read_model(f)
+    
+    # read state id
+    s <- read_state_id(f)
+    
+    # read file 
+    df <- readRDS(f) 
+    
+    # filter data
+    df <- df %>%
+      get_n_ahead() %>%
+      unnest(cols = c("data")) %>%
+      mutate(state_id = tolower(s),
+             variable = m) %>%
+      unnest(cols = c("forecast")) 
+    
+    # compute incidence
+    if (grepl("epi", m)) {
+      df <- df %>%
+        rename(m = forecast) %>%
+        comp_inc(m) %>%
+        rename(forecast = m) %>%
+        dplyr::select(-pop)
+    }
+    
+    return(df)
+  }))
+  
+  df_F <- df_F %>%
+    mutate(forecast = ifelse(forecast > max_inc, max_inc, forecast)) %>% 
+    mutate(variable = recode(variable, !!! model_names)) %>%
+    rename(value = forecast, target = incidence)
 }
 
 # add n_ahead
@@ -52,15 +54,6 @@ add_n_ahead <- function(D) {
   })
   return(D)
 }
-
-
-# add_n_ahead <- function(D) {
-#   D %>%
-#     group_by_at(c("state_id", "forecast_date")) %>%
-#     arrange(test_date) %>%
-#     mutate(n = 1:n()) %>%
-#     ungroup() 
-# }
 
 # compute incidence
 cc <- county_census %>%
@@ -176,6 +169,7 @@ plot_predict <- function(
   interval = TRUE # plot with uncertainty intervals
 ) {
   
+  # smoothing
   if (!is.null(smoothing)) {
     group_vars <- "variable"
     if (!interval) { group_vars = c(group_vars, "n") }
@@ -189,21 +183,20 @@ plot_predict <- function(
       na.omit()
   }
   
-  
-  dat_pred <- dplyr::filter(dat, variable != "target")
+  # observed data
   dat_obse <- dat %>%
-    dplyr::filter(variable == "target") %>%
-    dplyr::select(date, value) %>%
+    dplyr::select(date, target) %>%
     group_by(date) %>%
     slice(1) %>%
-    ungroup()
+    ungroup() %>%
+    rename(value = target)
   
   if (interval) {
     
     # plot interval
     pl <- ggplot(mapping = aes(x = date, y = value)) +
-      stat_lineribbon(data = dat_pred, .width = c(.95, .8, .5), color = "#08519C") +
-      geom_line(data = dat_obse, size = 1) +
+      stat_lineribbon(data = dat, .width = c(.95, .8, .5), color = "#08519C") +
+      geom_line(data = dat_obse) +
       scale_fill_brewer() +
       scale_x_date(expand = c(0,0), breaks = "2 months", date_labels = "%b %y") +
       facet_wrap(~ variable, ncol = 2) +
@@ -214,7 +207,7 @@ plot_predict <- function(
     
     # plot point estimate
     pl <- ggplot(mapping = aes(x = date, y = value)) +
-      geom_line(data = dat_pred, mapping = aes(color = n)) +
+      geom_line(data = dat, mapping = aes(color = n)) +
       geom_line(data = dat_obse, color = "black") +
       facet_wrap(~ variable, ncol = 2) +
       scale_color_viridis_d() +
@@ -263,228 +256,5 @@ plot_score <- function(
     theme_bw2() 
 
   return(pl)
-  
-}
-
-# regret <- function(o, f, n, w, b = 0.5) {
-#   e <- f - o
-#   we <- w[n] * e
-#   bwe <- ifelse(e < 0, (1+b) * we, (1-b) * we)
-#   abwe <- abs(bwe)
-#   return(abwe)
-# }
-# 
-# regret_score <- function(
-#   X, # predicted target
-#   y, # observed target
-#   pop = NULL, # population
-#   ... # additional arguments to function regret
-# ) {
-#  
-#   if (!is.null(pop)) {
-#     X <- trans(X, pop)#, transfct = NULL)
-#     y <- trans(y, pop)#, transfct = NULL)
-#   }
-#   
-#   n_days <- nrow(X)
-#   
-#   r <- numeric(n_days)
-#   for (i in 1:n_days) {
-#     r[i] <- mean(regret(X[i, ], y[i], n = i, ...))
-#   }
-#   
-#   war <- sum(r)
-#   
-#   return(war)
-# }
-
-is_peak <- function(
-  x, # target
-  through = F, # look for through instead of peak
-  t = 21, # number of days to the left and right to determine peak
-  min_x = 30, # minimum target value
-  na_value = F # value for resulting Nas
-) {
-  
-  n <- length(x)
-  isp <- logical(n)
-  isp[1:t] <- na_value
-  isp[(n-t+1):n] <- na_value
-  for (i in (t):(n-t)) {
-    left_x <- x[(i-t):(i-1)]
-    right_x <- x[(i+1):(i+t)]
-    if (through) {
-      extreme <- all(c(left_x, right_x) > x[i]) & x[i] > min_x
-    } else {
-      extreme <- all(c(left_x, right_x) < x[i]) & x[i] > min_x
-    }
-    if (extreme) {
-      isp[i] <- T
-    } else {
-      isp[i] <- F
-    }
-  } 
-  
-  return(isp)
-  
-}
-
-# closest_peak <- function(
-#   x, # target
-#   p, # peaks of target
-#   ... # additional arguments to is_peak(x, ...)
-# ) {
-#   
-#   # n
-#   n <- length(x)
-#   n_p <- sum(p)
-#   p_i <- which(p)
-#   
-#   # peaks of x
-#   p_x <- is_peak(x, ...)
-#   p_x_i <- which(p_x)
-#   
-#   # determine peak no of closest peak of x to peak of target p
-#   p_x_no <- rep(NA, n)
-#   
-#   for (i in 1:n_p) {
-#     d_i <- dist(c(p_i[i], p_x_i)) %>% as.matrix() %>% .[1,2:ncol(.)]
-#     idx_min_d_i <- which.min(d_i)
-#     idx_p_x_no <- p_x_i[idx_min_d_i]
-#     p_x_no[idx_p_x_no] <- i 
-#   }
-#   
-#   return(p_x_no)
-# }
-
-
-
-is_closest_peak <- function(
-  x, # forecast
-  p, # peaks of target
-  t_right = 28, # maximum time to look for peak to the right
-  t_left = 14 # maximum time to look for peak to the right
-) {
-  
-  # n
-  n <- length(x)
-  n_p <- sum(p)
-  
-  # next peaks
-  next_p <- rep(F, n)
-  
-  # split by peaks
-  btw_peaks <- split(x, cumsum(p))
-  btw_peaks_idx <- split(1:n, cumsum(p))
-  
-  # determine closest peak
-  for (k in 1:n_p) {
-    peak_left_x <- max(btw_peaks[[k]])
-    peak_left_idx <- which.max(btw_peaks[[k]])
-    peak_right_x <- max(btw_peaks[[k+1]])
-    peak_right_idx <- which.max(btw_peaks[[k+1]])
-    
-    is_left_close <- ((length(btw_peaks[[k]]) - peak_left_idx) < t_left)
-    is_right_close <- (peak_right_idx < t_right)
-    is_right_larger <- (peak_left_x <= peak_right_x)
-    
-    if (is_left_close & is_right_close) {
-      if (is_right_larger) {
-        next_p[btw_peaks_idx[[k+1]][peak_right_idx]] <- T
-      } else {
-        next_p[btw_peaks_idx[[k]][peak_left_idx]] <- T
-      }
-    } else if (is_left_close) {
-      next_p[btw_peaks_idx[[k]][peak_left_idx]] <- T
-    } else if (is_right_close) {
-      next_p[btw_peaks_idx[[k+1]][peak_right_idx]] <- T
-    }
-    # else, no close peak found
-    
-  }
-  
-  return(next_p)
-}
-
-
-is_critical <- function(
-  x, # target
-  critical_x = 50, # critical incidence
-  ... # additional arguments to is_peak
-) {
-  
-  # determine peaks 
-  peaks <- is_peak(x, ...)
-  
-  # determine bottoms
-  bottoms <- rep(F, length(peaks))
-  btw_peaks <- split(x, cumsum(peaks))
-  n <- 0
-  for (i in 1:length(btw_peaks)) {
-    if (i %% 2 == 0) {
-      bottoms[n + which.min(btw_peaks[[i]])] <- T
-    } else {
-      n <- n + length(btw_peaks[[i]])
-    }
-  }
-  
-  # combine peaks and bottoms to know what to know what to look for
-  extremes <- mapply(any, peaks, bottoms)
-  is_up <- ifelse(((1 + cumsum(extremes)) %% 2) == 0, F, T)
-  is_up <- sapply(split(is_up, cumsum(extremes)), all)
-  splits <- lag(cumsum(extremes)) # lag to move extremes to previous vector
-  splits[is.na(splits)] <- 0
-  x_list <- split(x, splits)
-  
-  
-  # determine criticals
-  criticals <- lapply(x_list, function(xl) rep(NA, length(xl)))
-  for (i in 1:length(x_list)) {
-    if (is_up[[i]]) { criticals[[i]][which(x_list[[i]] >= critical_x)[1]] <- "up" }
-    else {criticals[[i]][which(x_list[[i]] < critical_x)[1]] <- "down"}
-  }
-  
-  # add x (helpful for testing)
-  criticals <- unlist(criticals)
-  names(criticals) <- x
-  
-  return(criticals)
-}
-
-
-is_onset <- function(
-  x, # target
-  onset_x = 10, # target onset incidence
-  ... # additional arguments to is_peak
-) {
-  
-  # get peaks
-  peaks <- is_peak(x, ...)
-  
-  # determine onset
-  x_seg <- split(x, cumsum(peaks))
-  x_seg_idx <- split(1:length(x), cumsum(peaks))
-  onsets <- integer(length(x_seg)-1)
-  # for each peak find the first time x is below onset_x
-  for (i in 1:(length(x_seg)-1)) {
-    x_seg_i <- rev(x_seg[[i]])
-    x_seg_idx_i <- rev(x_seg_idx[[i]]) 
-    n <- length(x_seg_i)
-    k <- 1
-    while(ifelse(is.na(x_seg_i[k]), F, x_seg_i[k]  > onset_x)) {
-      k <- k + 1
-    }
-    if (k <= n) {
-      onsets[i] <- x_seg_idx_i[k]
-    } else {
-      # if x always > onset_x take the middle between minimum and maximum x
-      onsets[i] <- floor( ( x_seg_idx_i[which.min(x_seg_i)] + which(peaks)[i] ) / 2 )
-    }
-  }
-  
-  iso <- rep(F, length(x))
-  iso[onsets] <- T
-  
-  return(iso)
   
 }
