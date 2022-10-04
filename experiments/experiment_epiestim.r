2e152b5deccc09a9cd947b8181bc12c442d97d9c
nbanho <nbanho@ethz.ch>
2022-10-04 15:35:58 +0200

    updating experiments for epieestim and prophet

---
+++ experiments/experiment_epiestim.r
@@ -0,0 +1,56 @@
+run_prediction_us <- function() {
+  
+  # settings
+  args <- commandArgs(trailingOnly = TRUE)
+  states <- unlist(stringr::str_split(args[1], "8"))
+  source("settings/defaults.r")
+  
+  # libraries 
+  library(tidyverse)
+  
+  # models
+  source("models/epiestim.r")
+  tau_window <- c(1,3,5,7,14,21)
+  
+  # states
+  if (grepl("all", states)) {
+    states <- unique(stringi::stri_extract(list.files("data/us-selected-states/"), regex = "\\w{2}")) 
+  } 
+  
+  for (j in 1:length(states)) {
+    
+    # train and data
+    state <- states[j]
+    train_df_state <- readRDS(paste0("data/us-selected-states/", state, "-train.rds")) 
+    test_df_state <- readRDS(paste0("data/us-selected-states/", state, "-test.rds"))
+    
+    # loop over forecasting dates
+    K <- nrow(train_df_state)
+    for (k in 1:K) {
+      
+      # train and forecast for each model
+      if (all(train_df_state$data[[k]]$incidence==0)) {
+        # insert 0 predictions
+        predicted_cori <- matrix(0, nrow = n_preds, ncol = n_draws)
+        for (tau in tau_window) {
+          test_df_state$data[[k]][[paste0("epiestim",tau)]] <- get_samples(predicted_cori, test_df_state$data[[k]], k)
+        }
+      } else {
+        for (tau in tau_window) {
+          test_df_state$data[[k]][[paste0("epiestim",tau)]] <- get_samples(
+            train_and_predict.epiestim(tau = tau, data = train_df_state$data[[k]], 
+                                       seed = seed12345, n = n_preds, d = n_draws),
+            test_df_state$data[[k]], k
+          )
+        }
+      }
+    }
+    
+    # save
+    saveRDS(test_df_state, paste0("experiments/epiestim/", state, ".rds"))
+  }
+  
+}
+
+
+run_prediction_us()
---
+++ experiments/experiment_prophet.r
@@ -0,0 +1,56 @@
+run_prediction_us <- function() {
+  
+  # settings
+  args <- commandArgs(trailingOnly = TRUE)
+  states <- unlist(stringr::str_split(args[1], "8"))
+  source("settings/defaults.r")
+  
+  # libraries 
+  library(tidyverse)
+  
+  # models
+  source("models/prophet.r")
+  cp_scale <- seq(0.05, 0.45, 0.05)
+  
+  # states
+  if (grepl("all", states)) {
+    states <- unique(stringi::stri_extract(list.files("data/us-selected-states/"), regex = "\\w{2}")) 
+  } 
+  
+  for (j in 1:length(states)) {
+    
+    # train and data
+    state <- states[j]
+    train_df_state <- readRDS(paste0("data/us-selected-states/", state, "-train.rds")) 
+    test_df_state <- readRDS(paste0("data/us-selected-states/", state, "-test.rds"))
+    
+    # loop over forecasting dates
+    K <- nrow(train_df_state)
+    for (k in 1:K) {
+      
+      # train and forecast for each model
+      if (all(train_df_state$data[[k]]$incidence==0)) {
+        # insert 0 predictions
+        predicted_prophet <- matrix(0, nrow = n_preds, ncol = n_draws)
+        for (tau in cp_scale) {
+          test_df_state$data[[k]][[paste0("prophet",tau)]] <- get_samples(predicted_prophet, test_df_state$data[[k]], k)
+        }
+      } else {
+        for (tau in cp_scale) {
+          test_df_state$data[[k]][[paste0("prophet",tau)]] <- get_samples(
+            train_and_predict.prophet(cp_scale = tau, data = train_df_state$data[[k]], 
+                                      seed = seed12345, n = n_preds, d = n_draws),
+            test_df_state$data[[k]], k
+          )
+        }
+      }
+    }
+    
+    # save
+    saveRDS(test_df_state, paste0("experiments/prophet/", state, ".rds"))
+  }
+  
+}
+
+
+run_prediction_us()
